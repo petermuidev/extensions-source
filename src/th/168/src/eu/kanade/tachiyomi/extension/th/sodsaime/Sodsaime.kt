2f359b49511bb47b2c5de760f8a888ba32aeed72
petermuidev <77678315+petermuidev@users.noreply.github.com>
2024-07-22 22:42:42 +0700

    Add files via upload

---
+++ src/th/168/src/eu/kanade/tachiyomi/extension/th/sodsaime/Sodsaime.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.th.manga168
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class Manga168 : MangaThemesia("Manga168", "https://manga168.com", "th", dateFormat = SimpleDateFormat("MMMMM dd, yyyy", Locale("th")))
